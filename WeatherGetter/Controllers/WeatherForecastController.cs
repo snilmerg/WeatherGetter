bbb6e8ecda3be254aec37b00c935b71acd30e859
Maciej <mpiorek95@gmail.com>
2021-02-24 15:25:32 +0100

    WeatherCronService adjusted

--- WeatherGetter/Controllers/WeatherForecastController.cs
+++ WeatherGetter/Controllers/WeatherForecastController.cs
@@ -1,9 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
-using System.IO;
-using WeatherGetter.Models;
 
 namespace WeatherGetter.Controllers
 {
@@ -11,7 +8,7 @@ namespace WeatherGetter.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
-        private static readonly string ApiKey = "4f0845fe26db4203813194134212302";
+        
         private readonly ILogger<WeatherForecastController> _logger;
         private readonly IMemoryCache _cache;
 
@@ -22,31 +19,16 @@ namespace WeatherGetter.Controllers
         }
 
         [HttpGet]
-        public JsonResult GetWeatherInWroclaw()
+        public JsonResult GetWeatherInWroclaw(string city)
         {
+            string result;
 
-            var request = (System.Net.HttpWebRequest)System.Net.WebRequest.Create(
-                "https://api.worldweatheronline.com/premium/v1/weather.ashx?key=4f0845fe26db4203813194134212302&q=Wroclaw&format=json&num_of_days=7");
-            request.Method = "GET";
-            request.ContentType = "application/json";
-            request.Accept = "application/json";
-
-            try
+            if (_cache.TryGetValue(city, out result))
             {
-                var httpResponse = (System.Net.HttpWebResponse)request.GetResponse();
-                using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
-                {
-                    var result = streamReader.ReadToEnd();
-
-                    var deserializedResult = JsonConvert.DeserializeObject<WeatherForecast>(result);
-
-                    return new JsonResult(deserializedResult);
-                }
+                return new JsonResult(result);
             }
-            catch (System.Net.WebException e)
-            {
-                return new JsonResult(e.Message);
-            }
+
+            return new JsonResult($"City {city} does not exist in our weather context. Try 'Wrocław', 'Warszawa', 'Poznań' or 'Łódź'.");
         }
     }
 }
--- WeatherGetter/Utilities/WeatherCronService.cs
+++ WeatherGetter/Utilities/WeatherCronService.cs
@@ -1,19 +1,26 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 using System;
+using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
 using WeatherGetter.Abstraction;
+using WeatherGetter.Models;
 
 namespace WeatherGetter.Utilities
 {
     public class WeatherCronService : CronJobService
     {
+        private readonly string ApiKey = "4f0845fe26db4203813194134212302";
         private readonly ILogger<WeatherCronService> _logger;
+        private readonly IMemoryCache _cache;
 
-        public WeatherCronService(IScheduleConfig<WeatherCronService> config, ILogger<WeatherCronService> logger)
+        public WeatherCronService(IScheduleConfig<WeatherCronService> config, ILogger<WeatherCronService> logger, IMemoryCache cache)
             : base(config.CronExpression, config.TimeZoneInfo)
         {
             _logger = logger;
+            _cache = cache;
         }
 
         public override Task StartAsync(CancellationToken cancellationToken)
@@ -25,6 +32,13 @@ namespace WeatherGetter.Utilities
         public override Task DoWork(CancellationToken cancellationToken)
         {
             _logger.LogInformation($"{DateTime.Now:hh:mm:ss} WeatherCronService is working.");
+
+            GetForecastForCity("Wroclaw", "Wrocław");
+            GetForecastForCity("Lodz", "Łódź");
+            GetForecastForCity("Warszawa", "Warszawa");
+            GetForecastForCity("Poznan", "Poznań");
+
+            _logger.LogInformation($"{DateTime.Now:hh:mm:ss} WeatherCronService completed.");
             return Task.CompletedTask;
         }
 
@@ -32,6 +46,32 @@ namespace WeatherGetter.Utilities
         {
             _logger.LogInformation("WeatherCronService is stopping.");
             return base.StopAsync(cancellationToken);
+        }
+
+        private void GetForecastForCity(string city, string cacheCityName)
+        {
+            var url = "https://api.worldweatheronline.com/premium/v1/weather.ashx?key=" + ApiKey + "&q=" + city + "&format=json&num_of_days=7";
+            var request = (System.Net.HttpWebRequest)System.Net.WebRequest.Create(url);
+            request.Method = "GET";
+            request.ContentType = "application/json";
+            request.Accept = "application/json";
+
+            try
+            {
+                var httpResponse = (System.Net.HttpWebResponse)request.GetResponse();
+                using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+                {
+                    var result = streamReader.ReadToEnd();
+
+                    var deserializedResult = JsonConvert.DeserializeObject<WeatherForecast>(result);
+
+                    _cache.Set(cacheCityName, deserializedResult);
+                }
+            }
+            catch (System.Net.WebException e)
+            {
+                _logger.LogError(e.Message);
+            }
         }
     }
 }
